8e60a37952e391f64178b8ceb2074b1be337c8ea
oliveradk <40397426+oliveradk@users.noreply.github.com>
2020-10-02 09:17:56 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pure-python-brl-oliveradk", # Replace with your own username
+    version="0.0.1",
+    author="Oliver Daniels-Koch",
+    description="pure python bayesian rule list classifier",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
