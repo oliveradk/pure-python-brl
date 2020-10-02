33bc7f51dcca35a5762874e36a53bb6ed4c4b598
Oliver Daniels-Koch <odanielskoch@cra.lan@odanielskoch-ub.cra.lan>
2020-10-02 09:33:22 -0400

    removed 'oliveradk' from name

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="pure-python-brl-oliveradk", # Replace with your own username
+    name="pure-python-brl", # Replace with your own username
     version="0.0.1",
     author="Oliver Daniels-Koch",
     description="pure python bayesian rule list classifier",
